530d1c21e9ff8ae251e5ce44f16b26925be07f13
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-03-26 15:58:52 +0300

    added javascript functionality

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const display = document.getElementById(display);
-const btn = document.getElementById(btn);
+    function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    if (value === 'AC') {
+        display.value = '';
+    } else if (value === 'C') {
+        display.value = display.value.slice(0, -1);
+    } else if (value === '=') {
+        try {
+            display.value = eval(display.value);
+        } catch (e) {
+            display.value = 'Error';
+        }
+    } else {
+        display.value += value;
+    }
+}
+
+
+
 
